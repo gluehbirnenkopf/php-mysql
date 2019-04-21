7128c874c02c7eb93b2336e881f187d44ccc5c7d
Leon Gregori <mail@leongregori.com>
2019-04-21 08:55:43 +0200

    change format

--- db/initChat.sql
+++ db/initChat.sql
@@ -1,1 +1,5 @@
-CREATE TABLE chat(id INT AUTO_INCREMENT,name VARCHAR(255) NOT NULL,msg VARCHAR(255),date DATETIME,PRIMARY KEY (id));
+CREATE TABLE chat(id INT AUTO_INCREMENT,
+                  name VARCHAR(255) NOT NULL,
+                  msg VARCHAR(255),
+                  date DATETIME,
+                  PRIMARY KEY (id));
